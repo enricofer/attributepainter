1f44f11b80be757b94365ff9d56c052ef7f5cdf9
Enrico Ferreguti <enricofer@gmail.com>
2022-01-12 09:44:32 +0100

    tests implementation

---
+++ test/test_ap_dockwidget.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+"""DockWidget test.
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
+__author__ = 'ef@ef.ef'
+__date__ = '2022-01-12'
+__copyright__ = 'Copyright 2022, enricofer@gmail.com'
+
+import unittest
+
+from qgis.PyQt.QtGui import QDockWidget
+
+from ap_dockwidget import apDockWidget
+
+from utilities import get_qgis_app
+
+QGIS_APP = get_qgis_app()
+
+
+class apDockWidgetTest(unittest.TestCase):
+    """Test dockwidget works."""
+
+    def setUp(self):
+        """Runs before each test."""
+        self.dockwidget = apDockWidget(None)
+
+    def tearDown(self):
+        """Runs after each test."""
+        self.dockwidget = None
+
+    def test_dockwidget_ok(self):
+        """Test we can click OK."""
+        pass
+
+if __name__ == "__main__":
+    suite = unittest.makeSuite(apDialogTest)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
+
